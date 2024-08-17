df160a33e5418a40401cc966b1c73f3ff1e9aa91
Peterson <petersonvieira2412@gmail.com>
2024-08-17 20:24:51 -0300

    deploy.yml

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [data, setData] = useState(null);
+    const [data, setData] = useState(null);
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api')
-        .then(response => response.json())
-        .then(data => setData(data.message));
-  }, []);
+    useEffect(() => {
+        const apiUrl = process.env.REACT_APP_API_URL;
+        fetch(`${apiUrl}/api`)
+            .then(response => response.json())
+            .then(data => setData(data.message))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
 
-  return (
-      <div>
-        <h1>{data ? data : "Loading..."}</h1>
-      </div>
-  );
+    return (
+        <div>
+            <h1>{data ? data : "Loading..."}</h1>
+        </div>
+    );
 }
 
 export default App;
